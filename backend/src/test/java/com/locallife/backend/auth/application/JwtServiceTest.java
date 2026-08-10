36c16b273a254bc9690577f8b87968bdfe397fbb
Basset <alexbasset8@gmail.com>
2026-08-10 07:48:23 +0200

    Fix last error mvn verify : spotless:apply

--- backend/src/test/java/com/locallife/backend/auth/application/JwtServiceTest.java
+++ backend/src/test/java/com/locallife/backend/auth/application/JwtServiceTest.java
@@ -2,7 +2,6 @@ package com.locallife.backend.auth.application;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.locallife.backend.user.domain.Role;
 import io.jsonwebtoken.Claims;
